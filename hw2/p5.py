d2aeb2d42226dc833896faf86a7ece93c5326f26
Haunter17 <haunterhacker17@gmail.com>
2016-10-02 20:21:54 -0700

    shuffled

--- hw2/p5.py
+++ hw2/p5.py
@@ -1,23 +1,42 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
+import random
+import copy
 
 img = Image.open("/Users/FernandoWang/Downloads/hw2pr5.jpg")
 
 imgMat = np.array(list(img.getdata(band=0)), float)
 imgMat.shape = (img.size[1], img.size[0])
 imgMat = np.matrix(imgMat)
-plt.imshow(imgMat, cmap='gray')
-plt.show()
+# plt.imshow(imgMat, cmap='gray')
+# plt.show()
 
 U, sigma, V = np.linalg.svd(imgMat)
+print sigma
 
-kArr = [2, 10, 20]
-
-reconstimg = np.matrix(U[:,:1] * np.diag(sigma[:1]) * np.matrix(V[:1, :]))
 
 def plotGen():
 	plt.figure(1)
+	plt.style.use('ggplot')
+	# plt.subplot(211)
+	orderedPlot, = plt.plot(range(100), sigma[:100], 'bD')
+	plt.title('singular values')
+
+	shuffledSigma = copy.deepcopy(sigma)
+	random.shuffle(shuffledSigma)
+	# shuffledSigma = np.matrix(shuffledSigma)
+	# print shuffledSigma
+	# reconstimg = np.matrix(U[:,:499] * np.diag(shuffledSigma[:499]) * np.matrix(V[:499,:]))
+	# plt.subplot(211)
+	shuffledPlot, = plt.plot(range(100), shuffledSigma[:100], 'r^')
+	plt.legend((orderedPlot, shuffledPlot), ("100 largest","shuffled"), loc=1)
+	# plt.imshow(reconstimg, cmap='gray')
+
+
+
+	plt.figure(2)
+	plt.rcParams.update(plt.rcParamsDefault)
 	# plt.style.use('ggplot')
 	plt.subplot(221)
 	plt.imshow(imgMat, cmap='gray')
