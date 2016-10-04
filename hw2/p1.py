64d4ca00ff34709e041ea371ac87f1338e802e67
Haunter17 <haunterhacker17@gmail.com>
2016-10-03 19:08:39 -0700

    adding c and d

--- hw2/p1.py
+++ hw2/p1.py
@@ -34,6 +34,7 @@ def generateReportA():
 	fig = plt.figure()
 	ax = fig.add_subplot(111, projection='3d')
 	ax.plot_surface(X1, X2, normDensitylist,cmap=cm.coolwarm,linewidth=0, antialiased=False)
+	plt.title('Marginal distribution of p(x1)')
 
 	plt.show()
 
@@ -55,8 +56,59 @@ def generateReportB():
 	fig = plt.figure()
 	plt.style.use('ggplot')
 	normDensityPlot, = plt.plot(X1, normDensitylist, 'r')
+	plt.title('Marginal distribution of p(x2)')
 
+	plt.show()
+
+def generateReportC():
+	mu1 = np.matrix([[0], [0]])
+	mu2 = np.matrix([5])
+
+	sigma11 = np.matrix([[6, 8],[8, 13]])
+	sigma12 = np.matrix([[5], [11]])
+	sigma22 = np.matrix([14])
+	
+	X1 = np.linspace(-50.0, 10.0, num=1001)
+	X2 = np.linspace(-50.0, 10.0, num=1001)
+
+	cond_sigma = sigma11 - sigma12 * np.linalg.inv(sigma22) * sigma12.transpose()
+	normDensity = []
+	for x1 in X1:
+		xNorms = []
+		for x2 in X2:
+			cond_mu = mu1 + sigma12 * np.linalg.inv(sigma22) * (x2 - mu2)
+			xNorms.append((generateNormalDensity(np.matrix([[x1], [x2]]), cond_mu, cond_sigma)).item(0))	
+		normDensity.append(xNorms)
+
+	figure = plt.figure()
+	plot = figure.add_subplot(111, projection='3d')
+	plot.plot_surface(X1, X2, normDensity, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+
+	plt.show()
+
+def generateReportD():
+	mu1 = np.matrix([[0], [0]])
+	mu2 = np.matrix([5])
+
+	sigma11 = np.matrix([[6, 8],[8, 13]])
+	sigma12 = np.matrix([[5], [11]])
+	sigma22 = np.matrix([14])
+	
+	X1 = np.linspace(-10.0, 20.0, num=101)
+
+	cond_sigma = sigma22 - sigma12.transpose() * np.linalg.inv(sigma11) * sigma12
+	normDensity = []
+	for x1 in X1:
+		cond_mu = mu2 + sigma12.transpose() * np.linalg.inv(sigma11) * (x1 - mu1)
+		normDensity.append((generateNormalDensity(np.matrix([x1]), cond_mu, cond_sigma)).item(0))			
+			
+	figure = plt.figure()
+
+	normDensityPlot, = plt.plot(X1, normDensity, 'r')
+	print X1[normDensity.index(max(normDensity))]
 	plt.show()
 
 # generateReportA()
 # generateReportB()
+# generateReportC()
+generateReportD()
