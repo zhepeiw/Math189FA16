72ef96d7c537fb9f214eaeb4e524a39f46532494
Haunter17 <haunterhacker17@gmail.com>
2016-11-13 21:03:44 -0800

    finalized p5

--- hw4/p5.py
+++ hw4/p5.py
@@ -1,46 +1,41 @@
 import numpy as np
-import copy
 import matplotlib.pyplot as plt
+import os
+import shutil
 
-def NMF(V, r, max_iter=200):
+def NMF(V, r, max_iter=200, tolerance=1e-3, print_frq = 20):
 	n = V.shape[0]
 	m = V.shape[1]
 	obj = []
+
+	def genW(row, col, W, VHt, WHHt):
+		return W.item(row, col) * (VHt.item(row, col)) / (WHHt.item(row, col))
+
+	def genH(row, col, H, WtV, WtWH):
+		return H.item(row, col) * (WtV.item(row, col)) / (WtWH.item(row, col))
 	# W: n x r, H: r x m
 	W, H = np.mean(V) * np.matrix(np.random.rand(n, r)), np.mean(V) * np.matrix(np.random.rand(r, m))
-	for iter in range(max_iter):
-		Wcopy = copy.deepcopy(W)
-		Hcopy = copy.deepcopy(H)
-		# update H
-		for row in range(r):
-			for col in range(m):
-				# s = (W.transpose() * V)
-				# print row, col, (W.transpose() * W * H).shape, n, r
-				Hcopy[row, col] = H.item(row, col) * ((W.transpose() * V).item(row, col)) \
-				/ ((W.transpose() * W * H).item(row, col))
-		# update W
-		for row in range(n):
-			for col in range(r):
-				Wcopy[row, col] = W.item(row, col) * ((V * H.transpose()).item(row, col)) \
-				/ ((W * H * H.transpose()).item(row, col))
-		W, H = Wcopy, Hcopy
-		obj.append(dist(V, W * H))
+	for iteration in range(max_iter):
+		if iteration % print_frq == 0 and iteration != 0:
+			print("[iter={}] objective={}".format(iteration, obj[-1]))
+		VHt = V * H.transpose()
+		WHHt = W * H * H.transpose()
+		W = np.matrix([[genW(row, col, W, VHt, WHHt) for col in range(r)] for row in range(n)])
 
+		WtV = W.transpose() * V
+		WtWH = W.transpose() * W * H
+		H = np.matrix([[genH(row, col, H, WtV, WtWH) for col in range(m)] for row in range(r)])
+
+		d = dist(V, W * H)
+		obj.append(d)
+		if d <= tolerance:
+			break
 	
-	return W, H, [obj[i] for i in range(len(obj)) if i % 2 == 0]
+	return W, H, obj
 
 def dist(V, U):
-	assert(V.shape == U.shape)
-	diff = 0
-	for i in range(V.shape[0]):
-		for j in range(V.shape[1]):
-			diff += (V.item(i, j) - U.item(i, j))**2
-	return np.sqrt(diff)
-
-test = [[1.1,1.2,1.3,1.4], [1.8,1.7,0.6,0.5],[1.5,1.6,2.7,0.8], [2.3,2.2,1.1,1.4], [0.9,1.4,3.2,2.4]]
-V = np.matrix(test)
-W, H, obj = NMF(V, 3)
-print W
+	assert(V.shape == U.shape)	
+	return np.linalg.norm(V - U)
 
 def genCvgPlot(obj):
 	plt.style.use('bmh')
@@ -49,6 +44,75 @@ def genCvgPlot(obj):
 	plt.xlabel('# Iteration')
 	plt.ylabel('RMSE')
 	plt.plot(obj, 'r')
-	plt.savefig('confusion_5.pdf', format='pdf')
+	plt.savefig('cvg_5.pdf', format='pdf')
 	plt.show()
-# genCvgPlot(obj)
+
+def genFile():
+	dirname = "reuters21578"
+	filenames = sorted([os.path.join(dirname, fn) for fn in os.listdir(dirname) if fn.endswith(".sgm")])
+	output_dir = "samples"
+
+	count = 0
+
+	def save_content(content):
+		global count
+		name = os.path.join(output_dir, "samples-" + str(count) + ".txt")
+		count += 1
+		with open(name, 'w') as fn:
+			fn.write(content)
+
+	def parse_file(content):
+		i = 0
+		ans = []
+		while i < len(content):
+			j = i
+			while j < len(content) and content[j] != '<':
+				j += 1
+			if j + 6 <= len(content) and content[j:j + 6] == "<BODY>":
+				k = j + 6
+				while k < len(content) and content[k] != '<':
+					k += 1
+				if k + 7 <= len(content) and content[k:k + 7] == "</BODY>":
+					# Get rid of "\n&#3;"
+					e = k - 5
+					# Get rid of "\n Reuter"				
+					if content[e - 6:e].lower() == "reuter":
+						e -= 8
+					save_content(content[j + 6:e])
+				j = k + 7
+			i = j + 1
+	# Create output directory
+	if not os.path.exists(output_dir):
+	    os.makedirs(output_dir)
+	else:
+		shutil.rmtree(output_dir)
+
+	for fn in filenames:
+		with open(fn) as fd:
+			parse_file(fd.read())
+
+# process txt files
+CORPUS_PATH = os.path.join('samples')
+num_topics = 15
+num_top_words = 20
+filenames = sorted([os.path.join(CORPUS_PATH, fn) for fn in os.listdir(CORPUS_PATH)][:1000])
+import sklearn.feature_extraction.text as text
+vectorizer = text.CountVectorizer(input='filename', stop_words='english', decode_error='ignore', min_df=5)
+dtm = vectorizer.fit_transform(filenames).toarray()
+# print dtm.shape
+vocab = np.array(vectorizer.get_feature_names())
+
+def generateReport():
+	W, H, obj = NMF(dtm, num_topics)
+	H = np.array(H)
+	# print dtm.shape, H.shape
+	topic_words = []
+	for topic in H:
+		word_idx = np.argsort(topic)[::-1][0:num_top_words]
+		topic_words.append([vocab[i] for i in word_idx])
+	for t in range(len(topic_words)):
+		print("Topic {}: {}".format(t + 1, ' '.join(topic_words[t][:num_top_words])))
+	genCvgPlot(obj)
+
+generateReport()
+
